49817c292ad531b6e7c7e26f3b8f9cead2bae1b9
Roman Rey <rey.roman23@gmail.com>
2022-11-03 16:42:54 -0500

    commit jwt

--- src/datasources/mongo.datasource.ts
+++ src/datasources/mongo.datasource.ts
@@ -1,10 +1,11 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
+import {configuracion} from '../config/config';
 
 const config = {
   name: 'mongo',
   connector: 'mongodb',
-  url: 'mongodb+srv://adminMisionTic:Admin.2021@misiontic.jmslnyu.mongodb.net/test',
+  url: configuracion.cadenaBD,
   host: '',
   port: 0,
   user: '',
